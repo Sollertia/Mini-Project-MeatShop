a3ced640f7ae4e0ecdbe3db2bdf54665ea98422d
kancho06 <wotjd5792@gmail.com>
2021-10-20 14:39:57 +0900

    10/20 로컬url수정
    10/20 로컬url수정

--- src/main/java/com/hanghae/miniprojectmeatshop/service/ImageService.java
+++ src/main/java/com/hanghae/miniprojectmeatshop/service/ImageService.java
@@ -23,7 +23,6 @@ public class ImageService {
     private String uploadUrl;
 
     public Path saveFile(MultipartFile multipartFile) {
-        System.out.println("들옴");
         String extension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
         String fileName =
                 UUID.randomUUID().toString().substring(19) +
